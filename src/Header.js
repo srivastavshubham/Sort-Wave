09ee4dc05f4e2e7f52057db42c0cb37b6e854359
shubhan srivastava <shubhamda111@gmail.com>
2024-11-09 21:04:12 +0530

    create the structure of project

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React,{useState,useEffect} from 'react';
+
+function Header() {
+
+  return (
+    <div className="header">
+      <div className="header-logo">Sort<span>Wave</span></div>
+    
+   </div>
+  )
+}
+
+export default Header;
---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Sidebar from './Sidebar';
+
+function Home() {
+  return (
+    <div className="home">
+       <Sidebar />
+   </div>
+  )
+}
+
+export default Home;
